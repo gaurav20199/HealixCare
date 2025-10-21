4cd3f8d4fb0cd9a6f86e0180f3d445adaa14bd3d
Gaurav <gauravjoshi20199@gmail.com>
2025-10-21 14:05:34 +0530

    Added filter and exception. Added filter criteria in prod yml file

---
+++ api-gateway/src/main/java/com/project/apigateway/filter/JwtValidationGatewayFilterFactory.java
@@ -0,0 +1,46 @@
+package com.project.apigateway.filter;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Component
+public class JwtValidationGatewayFilterFactory extends
+    AbstractGatewayFilterFactory<Object> {
+
+  private static final Logger log = LoggerFactory.getLogger(JwtValidationGatewayFilterFactory.class);
+  private final WebClient webClient;
+
+  public JwtValidationGatewayFilterFactory(WebClient.Builder webClientBuilder,
+      @Value("${auth.service.url}") String authServiceUrl) {
+    this.webClient = webClientBuilder.baseUrl(authServiceUrl).build();
+  }
+
+  @Override
+  public GatewayFilter apply(Object config) {
+    return (exchange, chain) -> {
+      String token =
+          exchange.getRequest().getHeaders().getFirst(HttpHeaders.AUTHORIZATION);
+
+      log.info("token: {}", token);
+
+      if(token == null || !token.startsWith("Bearer ")) {
+        exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
+        return exchange.getResponse().setComplete();
+      }
+      log.info("Token format is valid");
+      return webClient.get()
+          .uri("/validate")
+          .header(HttpHeaders.AUTHORIZATION, token)
+          .retrieve()
+          .toBodilessEntity()
+          .then(chain.filter(exchange));
+    };
+  }
+}
